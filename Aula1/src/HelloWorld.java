8eccb0dbe604d64b7dac374121a13b38ef1c231b
dspaa1-iscteiul <dspaa1@iscte-iul.pt>
2019-10-03 22:49:38 +0100

    Teste

--- Aula1/src/HelloWorld.java
+++ Aula1/src/HelloWorld.java
@@ -9,9 +9,9 @@ import javax.swing.JTextField;
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		System.out.println("Hello World");
 		JFrame text = new JFrame();
 		text.add(new JTextField("Hello World"));
-		System.out.println("Hello World");
 		text.pack();
 		text.setLocationRelativeTo(null);
 		text.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
